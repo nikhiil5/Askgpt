78c9aa73a484dbcc6e592f98ee67114f5b0d5a69
nikhil <nikhilsingh141141@gmail.com>
2023-09-07 03:06:32 +0530

    mobileresponsivenavbar

--- frntend/src/components/Nav.js
+++ frntend/src/components/Nav.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Nav.css';
 
 const Nav = () => {
-    return(
+
+    const [drpdown, setdrpdown] = useState(false);
+    const changedropdown = () => setdrpdown(!drpdown);
+
+
+    return (
         <div className='Navbar'>
             <div className='elements'>
                 <ul className='Navcontainer'>
@@ -10,7 +15,7 @@ const Nav = () => {
                         <a href="https://askgpt-gamma.vercel.app/" className='Askgpt'>Askgpt</a>
                     </li>
                     <li className='item2'>
-                        <a href="https://platform.openai.com/" target="blank"className='Home'>API</a>
+                        <a href="https://platform.openai.com/" target="blank" className='API'>API</a>
                     </li>
                     <li className='item3'>
                         <a href="#" className='Features'>Features</a>
@@ -19,8 +24,35 @@ const Nav = () => {
                         <a href="#" className='ContactUs'>Contact Us</a>
                     </li>
                 </ul>
+                <ul className='mobilenavcontainer'>
+                    <li className='item1'>
+                        <a href="https://askgpt-gamma.vercel.app/" className='Askgpt'>Askgpt</a>
+                    </li>
+                    <li className='item2'>
+                        <button className={drpdown ? "notactive" : "active"} onClick={changedropdown}>
+                            ☰
+                        </button>
+                        <div className={drpdown ? "active" : "notactive"}>
+                            <button className='closedropdown' onClick={changedropdown}>
+                                ✕
+                            </button>
+                            <div className='dropdowncontent'>
+                                <button className='item3'>
+                                    <a href="https://platform.openai.com/" target="blank" className='API'>API</a>
+                                </button>
+                                <button className='item4'>
+                                    <a href="#" className='Features'>Features</a>
+                                </button>
+                                <button className='item5'>
+                                    <a href="#" className='ContactUs'>Contact Us</a>
+                                </button>
+                            </div>
+
+                        </div>
+                    </li>
+                </ul>
             </div>
-        
+
         </div>
     );
 }
